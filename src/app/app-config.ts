49541640c49f9aca65a273cf3fac1900567f0d76
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-04-12 14:08:51 -0400

    changes to token

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -1,5 +1,12 @@
 import { OpaqueToken } from '@angular/core';
 
+export const GUID = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+  return v.toString(16);
+});
+
+export const CURRENT_AUTHOR_ID = new OpaqueToken('some_guid');
+
 export interface IAppConfig {
   maxCommercialOccupancy: number;
   maxResidentialOccupancy: number;
