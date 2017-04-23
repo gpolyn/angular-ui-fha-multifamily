d9c919d7e52bdd8a80377c62e3234555bd1f9e32
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-04-23 11:37:35 -0400

    updated apartment component config

--- src/app/income/apartment/config.ts
+++ src/app/income/apartment/config.ts
@@ -2,7 +2,7 @@ import { OpaqueToken } from '@angular/core';
 import { IApartmentIncome } from './apartment-income';
 
 export const INITIAL_CONFIG: IApartmentIncome = {
-  bedrooms: undefined,
+  bedrooms: 0,
   squareFeet: undefined,
   monthlyRent: undefined,
   units: undefined,
