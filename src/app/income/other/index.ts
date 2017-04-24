0ca1730b3ef8ea5629b9143acffed3f07042a5f5
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-04-24 17:18:27 -0400

    minor change to other income component

--- src/app/income/other/index.ts
+++ src/app/income/other/index.ts
@@ -2,3 +2,4 @@ export * from './services/other-income.service';
 export * from './other-income.module';
 export * from './config/config';
 export * from './interfaces/other-income.interface';
+export * from './other-income.module';
