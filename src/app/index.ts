11043834ee6e452a790f7102d7ce277b79654274
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-05-03 10:32:52 -0400

    moved input-and-checkbox to components

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,18 +2,9 @@ import {NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import {AppComponent} from './containers/app.component';
-
-import { InputAndCheckboxModule} from './input-and-checkbox';
-//import { OtherIncomeWrapperModule} from './income/other-income-wrapper.module';
-//import { ApartmentIncomeFacadeModule} from './income/apartment-income-facade.module';
 import { OtherIncomeFacadeModule, ApartmentIncomeFacadeModule, EffectiveIncomeFacadeModule, OpexFacadeModule } from './component-module-facades';
-//import { ParkingIncomeWrapperModule} from './income/parking-income-wrapper.module';
 import { ParkingIncomeFacadeModule} from './component-module-facades';
-
-
 import { LoanCharacteristicsModule, ProjectCharacteristicsModule}         from './components';
-import {HeroDetailComponent1} from './input-and-checkbox/input-and-checkbox.component';
-import {CounterInputComponent} from './input-and-checkbox/textfield-and-checkbox.component';
 
 import { CommercialIncomeService, 
          ResidentialIncomeService, 
@@ -38,13 +29,11 @@ import { DI_CONFIG, APP_CONFIG, CURRENT_AUTHOR_ID, GUID } from './config';
     //FormsModule,
     //ReactiveFormsModule,
     ProjectCharacteristicsModule,
-    //ParkingIncomeWrapperModule,
     ParkingIncomeFacadeModule,
     ApartmentIncomeFacadeModule,
     EffectiveIncomeFacadeModule,
     OpexFacadeModule,
     LoanCharacteristicsModule,
-    //OtherIncomeWrapperModule
     OtherIncomeFacadeModule
   ],
   providers: [
