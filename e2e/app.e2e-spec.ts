1e41fc7f8b0d29c28d35083e5af642f8db3262a4
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-05-04 12:50:52 -0400

    mod to e2e spec per github.com/DefinitelyTyped/DefinitelyTyped/issues/15215

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { HelloAngularCliPage } from './app.po';
+import { async } from '@angular/core/testing';
 
 describe('hello-angular-cli App', () => {
   let page: HelloAngularCliPage;
@@ -7,8 +8,10 @@ describe('hello-angular-cli App', () => {
     page = new HelloAngularCliPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display message saying app works', async() => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('FHA Sec. 223(f) refinance/purchase demo');
+    const subject = await page.getParagraphText();
+    //expect(page.getParagraphText()).toEqual('FHA Sec. 223(f) refinance/purchase demo');
+    expect(subject).toEqual('FHA Sec. 223(f) refinance/purchase demo');
   });
 });
